b2c873f252eb1edfa52988003f730faeb4cd976f
vitext <admin@pc-politika.cz>
2015-11-23 18:26:48 +0100

    Update admin_reset.php

--- locale/Czech/admin/admin_reset.php
+++ locale/Czech/admin/admin_reset.php
@@ -1,46 +1,32 @@
 <?php
 // Infusion titles & description
-$locale['apw_title'] = "Restart hesel administrátorů";
+$locale['apw_title'] = "Obnovení/restartování hesel administrátorů";
 $locale['apw_desc'] = "Mění přihlašovací/administrační hesla administrátorů";
-$locale['apw_admin1'] = "Restart Administrátora";
+$locale['apw_admin1'] = "Obnovení Administrátora";
 
-$locale['400'] = "Administrátoři k restartu:";
+$locale['400'] = "Administrátoři k obnovení:";
 $locale['401'] = "Všichni Administrátoři";
 $locale['402'] = "Pouze Hlavní Administrátoři";
 $locale['403'] = "Pouze Administrátoři";
-$locale['404'] = "Důvod restartu:";
-$locale['405'] = "Restartovat obě - administrační a přihlasovací - Hesla.";
-$locale['406'] = "Restartovat Hesla";
-$locale['407'] = "Heslo restartováno v ";
-$locale['408'] = "Vítejte [USER_NAME],
-
-Vaše administrační heslo na ".$settings['siteurl']." bylo restartováno:
-
-Administrační heslo: [NEW_ADMIN_PASS]
-
-Důvodem pro restartování hesla je: [RESET_MESSAGE]
-
-S pozdravem,
-[ADMIN]";
-$locale['409'] = "Vítejte [USER_NAME],
-
-Vaše přihlašovací a administrační heslo na ".$settings['siteurl']." bylo restartováno:
-
-Přihlašovací heslo: [NEW_PASS]
-Administrační heslo: [NEW_ADMIN_PASS]
-
-Důvodem pro restart je: [RESET_MESSAGE]
-
-S pozdravem,
-[ADMIN]";
-$locale['410'] = "Výsledky restartu";
+$locale['404'] = "Důvod obnovení:";
+$locale['405'] = "Obnovit administrační a přihlasovací - Hesla.";
+$locale['406'] = "Obnovit hesla";
+$locale['407'] = "Heslo obnoveno! ";
+$locale['408'] = "Vítejte [USER_NAME],\n\nVaše administrační heslo bylo obnoveno [SITEURL]:\n\n 
+Administrační heslo: [NEW_ADMIN_PASS].\n\nDůvodem pro obnovení hesla je: [RESET_MESSAGE]\n\nS pozdravem,\n[ADMIN]";
+$locale['409'] = "Vítejte [USER_NAME],\nVaše přihlašovací a administrační heslo bylo obnoveno na [SITEURL]:\n\n
+Přihlašovací heslo: [NEW_PASS]\n
+Administrační heslo: [NEW_ADMIN_PASS]\n\n
+Důvodem pro obnovení je: [RESET_MESSAGE]\n
+S pozdravem,\n[ADMIN]";
+$locale['410'] = "Výsledky obnovení";
 $locale['411'] = "<strong>Poznámka:</strong> Administrační heslo nebylo zadáno nebo je chybné.";
 $locale['412'] = "Administrační heslo:";
-$locale['415'] = "Záznam Restartů";
-$locale['416'] = "Záznam ID";
-$locale['417'] = "Datum restartu";
+$locale['415'] = "Obnovit přihlášení";
+$locale['416'] = "Přihlášení ID";
+$locale['417'] = "Datum obnovení";
 $locale['418'] = "Odpovědný";
-$locale['419'] = "Restartovaný uživatel";
+$locale['419'] = "Obnovený uživatel";
 $locale['420'] = "Úspěšně";
 $locale['421'] = "Důvod";
 $locale['422'] = "z";
